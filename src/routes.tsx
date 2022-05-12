b58124cec27e854bd8e94364ff5f8309da9ce340
Plinio <plininhodf@gmail.com>
2022-05-12 14:14:18 -0300

    Correçao da versao(react-router-dom)

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,17 +1,24 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 import Landing from "./pages/Landing";
 import TeacherList from "./pages/TeacherList";
 import TeacherForm from "./pages/theacherForm";
 
-function Routes() {
+const Routes = () => {
   return (
     <BrowserRouter>
-      <Route path="/"  element={ <Landing /> } />
-      <Route path="/study/*" element={ <TeacherList /> } />
-      <Route path="/give-classes/*" element={ <TeacherForm /> } />
+     <Switch>
+      <Route path="/" exact component={Landing} />
+      <Route path="/study" component={TeacherList} />
+      <Route path="/give-classes" component={TeacherForm} />
+     </Switch>
     </BrowserRouter>
   )
 }
 
 export default Routes;
+
+//npm uninstall react-router-dom@5.2
+//npm uninstall @types/react-router-dom
+//npm npm install react-router-dom@5.2
+//npm install @types/react-router-dom
