f735c74e23ddb8c4c98ef5003945301c873b890f
Plinio <plininhodf@gmail.com>
2022-05-12 11:56:27 -0300

    Layout inicial e Routes

---
+++ src/routes.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom"
+import Landing from "./pages/Landing";
+import TeacherList from "./pages/TeacherList";
+import TeacherForm from "./pages/theacherForm";
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Route path="/"  element={ <Landing /> } />
+      <Route path="/study/*" element={ <TeacherList /> } />
+      <Route path="/give-classes/*" element={ <TeacherForm /> } />
+    </BrowserRouter>
+  )
+}
+
+export default Routes;
